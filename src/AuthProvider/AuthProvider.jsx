a2a903b670f1f224e5d086055fdd9da28ccbb4b7
Jubayer husain <jubayerhusai360@gmail.com>
2024-11-13 12:08:44 +0600

    user login functionality done

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,23 +1,30 @@
 /* eslint-disable react/prop-types */
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { createContext } from "react";
 import { auth } from "../firebase/firebase.init";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext();
 function AuthProvider({ children }) {
-  //make func for create new user 
-  const createUser = (email, password) =>{
-    return createUserWithEmailAndPassword(auth , email, password)
-  }
- // make a object for all user context
- const authInfo = {
-  createUser,
- }
+  //make func for create new user
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  // make a func for login user
+  const loginUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  // make a object for all user context
+  const authInfo = {
+    createUser,
+    loginUser,
+  };
   return (
     <div>
-      <AuthContext.Provider value={authInfo}>
-        {children}
-        </AuthContext.Provider>
+      <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
     </div>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,22 @@
+import { AuthContext } from "../AuthProvider/AuthProvider";
+import { useContext } from 'react';
 function Login() {
+  const {loginUser} = useContext(AuthContext)
+  const hundleLogin = (event) =>{
+    event.preventDefault()
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+
+    // pass the value for user login 
+    loginUser(email, password)
+    .then(res => {
+      console.log(res.user);
+    })
+    .catch(error =>{
+      console.log("ERROR", error.message);
+    })
+  }
   return (
     <div className="bg-base-200 py-20">
       <div className="hero-content flex-col">
@@ -6,13 +24,14 @@ function Login() {
           <h1 className="text-3xl font-bold text-center">Sign In now!</h1>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form onSubmit={hundleLogin} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
+                name="email"
                 placeholder="email"
                 className="input input-bordered"
                 required
@@ -24,6 +43,7 @@ function Login() {
               </label>
               <input
                 type="password"
+                name="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
